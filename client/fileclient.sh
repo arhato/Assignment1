a3641736fb933a87bb3cc52bc5d6898019e1a2d4
Arrhat Maharjan <arhato23@gmail.com>
2025-03-20 19:41:13 +0000

    Corrected command for volume creation for client container.

--- client/fileclient.sh
+++ client/fileclient.sh
@@ -1,5 +1,5 @@
 SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' server-con)
-docker create volume clientvol
+docker volume create clientvol
 docker build -t client .
 docker run -it --name client-con --network network -v clientvol:/clientdata client "$SERVER_IP" 8080
 echo "Client is running. Waiting for file transfer..."
